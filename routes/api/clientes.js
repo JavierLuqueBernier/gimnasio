78f2b7584ddccaa1e172c4d52dbf585636b7eb73
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-16 00:52:15 +0100

    iniciacion de clientes.js tras los fallos en consola

---
+++ routes/api/clientes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const Cliente = require('../../models/cliente');
+
+//GET hhtp://localhost:3000/api/clientes
+router.get('/', async (req, res) => {
+    const rows = await Cliente.getAll();
+    res.json(rows);
+});
