abd1a8c6471e84390ec4d828a5aeafab45e614e1
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-16 00:04:41 +0100

    finalizacion ejercicios.js, con las mismas funciones que profesores.js

--- routes/api/ejercicios.js
+++ routes/api/ejercicios.js
@@ -8,4 +8,23 @@ router.get('/', async (req, res) => {
     res.json(rows);
 });
 
+// POST http://localhost:3000/api/ejercicios
+router.post('/', async (req, res) => {
+    const result = await Ejercicio.create(req.body);
+    res.json(result);
+});
+
+//PUT http://localhost:3000/api/ejercicios/:pEjercicioId
+router.put('/:pEjercicioId', async (req, res) => {
+    const result = await Ejercicio.updateById(req.body, req.params.pEjercicioId);
+    res.json(result)
+});
+
+// DELETE http://localhost:3000/api/ejercicios
+router.delete('/', async (req, res) => {
+    const result = await Ejercicio.deleteById(req.body.id);
+    res.json(result);
+});
+
+
 module.exports = router;
