00fc0efe3da2cf0e7f7a07813a70e22fc699f0b5
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-15 22:39:30 +0100

    creamos el archivo directorio api y lo configuramos, ademàs de enmpezar a configurarlo

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
+
+require('dotenv').config();
 
 var app = express();
 
@@ -25,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require ('mysql')
 
 exports.connect=()=>{
     const pool= mysql.createPool({
-        host:'127.0.0.1',
-        user:'root',
-        password:'',
-        port:3306,
-        database:'gimnasio02',
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        port: DB_PORT,
+        database: DB_DATABASE,
         });
 
     global.db=pool;
