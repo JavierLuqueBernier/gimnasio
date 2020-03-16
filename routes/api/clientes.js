6362b2b92905f714ee8e6c03a8678623264a3b43
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-16 16:05:20 +0100

    Consola no crashea a falta POST en clientes.js

--- routes/api/clientes.js
+++ routes/api/clientes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator')
 
 const Cliente = require('../../models/cliente');
 
@@ -6,6 +7,19 @@ const Cliente = require('../../models/cliente');
 router.get('/', async (req, res) => {
     const rows = await Cliente.getAll();
     res.json(rows);
+});
+
+//PUT http://localhost:3000/api/clientes/:pClienteId
+router.put('/:pClienteId', async (req, res) => {
+    const result = await Cliente.updateById(req.body, req.params.pClienteId);
+    res.json(result)
+});
+
+
+// DELETE http://localhost:3000/api/clientes
+router.delete('/', async (req, res) => {
+    const result = await Cliente.deleteById(req.body.id);
+    res.json(result);
 });
 
 module.exports = router;
