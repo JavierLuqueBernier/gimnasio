4e82f1c8ff1eccadba7d05522603a5f686395355
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-16 16:19:41 +0100

    Finalizacion de clientes.js

--- routes/api/clientes.js
+++ routes/api/clientes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require('express').Router(); //importante no olvidarse de poner esta linea
 const { check, validationResult } = require('express-validator')
 
 const Cliente = require('../../models/cliente');
@@ -7,6 +7,29 @@ const Cliente = require('../../models/cliente');
 router.get('/', async (req, res) => {
     const rows = await Cliente.getAll();
     res.json(rows);
+});
+
+// POST http://localhost:3000/api/clientes
+router.post('/', [
+    //validadores
+    check('nombre').isLength({ min: 3 }).isAlphanumeric(), //alphanumeric es para que solo se pueda utilizar letras, no numeros
+    check('apellidos').isLength({ min: 3 }).isAlphanumeric(),
+    check('direccion').isLength({ min: 3 }),
+    check('email').isEmail(),
+    check('edad').isNumeric(),
+    check('sexo').isLength({ min: 1 }),
+    check('cuota').isDecimal(),
+    check('fecha_nacimiento'),
+    check('dni').custom((value) => {
+        return (/^[a-zA-Z0-9]{5,10}$/).test(value);
+    })
+], async (req, res) => {
+    const errors = validationResult(req); // esta linea nos informa de si ha habido fallos en alguno de los middleware de arriba
+    if (!errors.isEmpty()) {
+        return res.status(422).json(errors.array())
+    }
+    const result = await Cliente.create(req.body);
+    res.json(result);
 });
 
 //PUT http://localhost:3000/api/clientes/:pClienteId
@@ -22,4 +45,4 @@ router.delete('/', async (req, res) => {
     res.json(result);
 });
 
-module.exports = router;
+module.exports = router; //Crasheaba por que faltaba esta linea, importante no olvidarse de ponerla
